f074bd1eb57090ccfad5a16c5cb999bacfde1754
Herase <phoebe7075@gmail.com>
2021-03-08 20:25:25 +0900

    idk
    idk idk idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Board from './components/board/board';
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
