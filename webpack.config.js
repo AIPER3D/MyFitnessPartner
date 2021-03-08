7b0e4807ad76a887ea568cc38f7e4b86e2eefeb2
ToTo-Mo <bum4496@naver.com>
2021-03-08 22:03:14 +0900

    Update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  target: 'electron-renderer',
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [[
-              '@babel/preset-env', {
-                targets: {
-                  esmodules: true
-                }
-              }],
-              '@babel/preset-react']
-          }
-        }
+        test: /\.ts/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')]
       },
       {
         test: [/\.s[ac]ss$/i, /\.css$/i],
@@ -29,18 +15,65 @@ module.exports = {
           // Creates `style` nodes from JS strings
           'style-loader',
           // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
+          'css-loader'
+          // // Compiles Sass to CSS
+          // 'sass-loader',
         ],
       }
     ]
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: [".js", ".json", ".jsx", ".ts",".tsx",".css"],
+    // extensions that are used
   },
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'build', 'js'),
-  },
-};
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath : '/dist'
+  }
+}
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/index.tsx',
+//   devtool: 'inline-source-map',
+//   target: 'electron-renderer',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: [[
+//               '@babel/preset-env', {
+//                 targets: {
+//                   esmodules: true
+//                 }
+//               }],
+//               '@babel/preset-react']
+//           }
+//         }
+//       },
+//       {
+//         test: [/\.s[ac]ss$/i, /\.css$/i],
+//         use: [
+//           // Creates `style` nodes from JS strings
+//           'style-loader',
+//           // Translates CSS into CommonJS
+//           'css-loader',
+//           // Compiles Sass to CSS
+//           'sass-loader',
+//         ],
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: ['.js'],
+//   },
+//   output: {
+//     filename: 'app.js',
+//     path: path.resolve(__dirname, 'build', 'js'),
+//   },
+// };
