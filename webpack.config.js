5fcebfaad518cd30363e523cbeb5ddefaf89691e
ToTo-Mo <bum4496@naver.com>
2021-03-08 23:36:21 +0900

    Update bable-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,28 @@ module.exports = {
   entry: './src/index.tsx',
   module: {
     rules: [
+      // {
+      //   test: /\.ts/,
+      //   use: 'ts-loader',
+      //   include: [path.resolve(__dirname, 'src')]
+      // },
       {
-        test: /\.ts/,
-        use: 'ts-loader',
-        include: [path.resolve(__dirname, 'src')]
+        test: /\.(ts|jsx|tsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript',
+              '@babel/preset-react'
+            ],
+            plugins: [
+              '@babel/proposal-class-properties',
+              '@babel/proposal-object-rest-spread'
+            ]
+          }
+        }
       },
       {
         test: [/\.s[ac]ss$/i, /\.css$/i],
@@ -19,11 +37,23 @@ module.exports = {
           // // Compiles Sass to CSS
           // 'sass-loader',
         ],
-      }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+          loader: 'file-loader'
+        }]
+      },
+      {
+        test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [{
+          loader: 'file-loader'
+        }]
+      },
     ]
   },
   resolve: {
-    extensions: [".js", ".json", ".jsx", ".ts",".tsx",".css"],
+    extensions: [".js", ".json", ".jsx", ".ts", ".tsx", ".css"],
     // extensions that are used
   },
   output: {
