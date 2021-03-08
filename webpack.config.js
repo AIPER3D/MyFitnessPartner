f2a1cc3dd2f26dd2b5827f2d45537ba327686ef7
ToTo-Mo <bum4496@naver.com>
2021-03-08 22:53:18 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -28,52 +28,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath : '/dist'
+    path: path.resolve(__dirname, 'public'),
+    // publicPath : '/dist'
   }
 }
-
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/index.tsx',
-//   devtool: 'inline-source-map',
-//   target: 'electron-renderer',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: [[
-//               '@babel/preset-env', {
-//                 targets: {
-//                   esmodules: true
-//                 }
-//               }],
-//               '@babel/preset-react']
-//           }
-//         }
-//       },
-//       {
-//         test: [/\.s[ac]ss$/i, /\.css$/i],
-//         use: [
-//           // Creates `style` nodes from JS strings
-//           'style-loader',
-//           // Translates CSS into CommonJS
-//           'css-loader',
-//           // Compiles Sass to CSS
-//           'sass-loader',
-//         ],
-//       }
-//     ]
-//   },
-//   resolve: {
-//     extensions: ['.js'],
-//   },
-//   output: {
-//     filename: 'app.js',
-//     path: path.resolve(__dirname, 'build', 'js'),
-//   },
-// };
