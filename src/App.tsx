784cb84a83e27a8a2cca4397faaf38214e64392f
SYN0P <reldn@naver.com>
2021-03-14 17:40:16 +0900

    Modify main page : Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
-import Board from './components/board/board';
-import Gallery from './components/board/gallery';
-import Calendar from './components/calendar/Calendar';
+import { Routines, Videos } from './pages';
 
 function App() {
 	return (
-		<div>
-			<Calendar />
-		</div>
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">메인 화면</Link>
+						</li>
+						<li>
+							<Link to="/routines">루틴 관리</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Switch>
+					<Route path="/routines">
+						<Routines />
+					</Route>
+					<Route path="/">
+						<h2>Home</h2>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
