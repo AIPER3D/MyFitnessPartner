0ed5806867fc677d1e256fc1b71e72ed0a0160d4
SYN0P <reldn@naver.com>
2021-03-15 02:51:32 +0900

    Remove DB Logic (Temporarily)

--- src/pages/routines.tsx
+++ src/pages/routines.tsx
@@ -1,11 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { Board, Content } from '../components/board';
 
-const level = require('level');
+import { createRxDatabase, addRxPlugin } from 'rxdb';
+import { RoutineSchema } from '../schema/routine';
 
 function Routines() {
+	/*
+	useEffect(() => {
+		(async () => {
+			addRxPlugin(require('pouchdb-adapter-idb'));
+
+			const db = await createRxDatabase({
+				name: 'data',
+				adapter: 'idb',
+			});
+
+			await db.addCollections({
+				routines: {
+					schema: RoutineSchema,
+				},
+			});
+
+			console.log(db['routines']);
+		})();
+	}, []);
+	*/
+	/*
+	(async () => {
+		addRxPlugin(require('pouchdb-adapter-leveldb'));
+		// const leveldown = require('leveldown');
+
+		// console.log(leveldown);
+		// if (!leveldown) console.log('1111');
+		// if (typeof leveldown.super_ !== 'function') console.log('2222');
+
+		const db = await createRxDatabase({
+			name: 'data',
+			adapter: leveldown,
+		});
+
+		await db.collection({
+			name: 'routines',
+			schema: require('../schema/routine.json'),
+		});
+
+		await db['routines'].insert({
+			routine_id: 0,
+			routine_name: 'Bob',
+		});
+
+		console.log(db);
+	})();
+
 	const [content, setContent] = useState<Content[]>([]);
-	const db = level('data', { valueEncoding: 'json' });
 
 	const ins = () => {
 		db.get('key', (err : any, value : any) => {
@@ -23,7 +70,6 @@ function Routines() {
 			});
 		});
 	};
-
 	const del = () => {
 		db.del('key', (err : any) => {
 			if (err) return console.error(err);
@@ -40,12 +86,13 @@ function Routines() {
 			console.log(value);
 		});
 	}, []);
+	*/
 
 	return (
 		<div className="App">
-			<Board title='루틴 관리1234' type='gallery' content={ content }/>
-			<input type='button' onClick={ ins } value='추가' />
-			<input type='button' onClick={ del } value='삭제' />
+			<Board title='루틴 관리1234' type='gallery' content={ [] }/>
+			<input type='button' value='추가' />
+			<input type='button' value='삭제' />
 		</div>
 	);
 }
