a5b5be598d0634701e1ba541ad3b98911cdfa7af
SYN0P <reldn@naver.com>
2021-03-22 18:55:13 +0900

    Create New Page

---
+++ src/pages/routineCreate.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { Header } from '../components/common';
+
+function RoutineCreate() {
+	return (
+		<div>
+			<Header text='나만의 루틴 만들기' />
+		</div>
+	);
+}
+
+export default RoutineCreate;
