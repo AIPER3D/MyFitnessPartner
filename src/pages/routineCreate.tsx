af8c094de87574573ad68d8d394b2aebf9e04731
SYN0P <reldn@naver.com>
2021-03-24 17:56:45 +0900

    Add DraggableList Component

--- src/pages/routineCreate.tsx
+++ src/pages/routineCreate.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Header } from '../components/common';
+import { DraggableList } from '../components/draggableList';
 
 function RoutineCreate() {
 	return (
 		<div>
 			<Header text='나만의 루틴 만들기' />
+			<DraggableList />
 		</div>
 	);
 }
