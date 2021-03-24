b2c6d27396260504451be8b3393086bbd287c01e
SYN0P <reldn@naver.com>
2021-03-24 13:28:56 +0900

    Add Upload Component

---
+++ src/components/file/upload.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type UploadProps = {
+
+};
+
+const Title = styled.div`
+
+`;
+
+function Upload() {
+	return (
+		<div>
+
+		</div>
+	);
+}
+
+Upload.defaultProps = {
+
+};
+
+export default Upload;
--- src/pages/videoCreate.tsx
+++ src/pages/videoCreate.tsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import { List, Content } from '../components/board';
 import { Header } from '../components/common';
+import { Upload } from '../components/file';
 
 function VideoCreate() {
+	const content : Content = {
+		id: 0,
+		title: '',
+	};
+
 	return (
 		<div >
 			<Header text='영상 등록' />
+			<Upload>
+				<List content={ content }></List>
+			</Upload>
 		</div>
 	);
 }
