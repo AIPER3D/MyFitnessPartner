1c05f932bfaf7a48afdfcf343d689a7bbf680cf4
fsnyd <52208468+fsnyd@users.noreply.github.com>
2021-04-12 15:00:13 +0900

    toy add tools to onnx use / wanna beom to test it

--- src/back/toy.ts
+++ src/back/toy.ts
@@ -1,5 +1,6 @@
 import { Tensor, InferenceSession } from "onnxjs";
-
+import ndarray from "ndarray";
+import ops from "ndarray-ops";
 class Toy
 {
     private AVG_PERSON_HEIGHT : number = 100;
@@ -8,9 +9,11 @@ class Toy
     private previous_poses_2d : any[] = [];
     
 
-    // function extract_poses(heatmaps:any[][],pafs:any[][] , upsample_ratio:number): any
+    // function extract_poses(heatmaps:any,pafs:any,upsample_ratio:number): any
     // {
-        
+       
+
+    //     heatmaps =
     // }
 
 
@@ -23,3 +26,40 @@ class Toy
 }
 
 export { Toy };
+
+    function preprocess(data:any, width:number, height:number) : any
+    {
+        const dataFromImage = ndarray(new Float32Array(data), [width, height, 4]);
+        const dataProcessed = ndarray(new Float32Array(width * height * 3), [1, 3, height, width]);
+    
+        // Normalize 0-255 to (-1)-1
+        ops.divseq(dataFromImage, 128.0);
+        ops.subseq(dataFromImage, 1.0);
+    
+        // Realign imageData from [224*224*4] to the correct dimension [1*3*224*224].
+        ops.assign(dataProcessed.pick(0, 0, null, null), dataFromImage.pick(null, null, 2));
+        ops.assign(dataProcessed.pick(0, 1, null, null), dataFromImage.pick(null, null, 1));
+        ops.assign(dataProcessed.pick(0, 2, null, null), dataFromImage.pick(null, null, 0));
+    
+        return dataProcessed.data ;
+    }
+
+    async function loadOnnx():Promise<any>
+    {
+        const session = new InferenceSession();
+        const url = "./src/models/human-pose-estimation-3d.onnx";
+        await session.loadModel(url);
+
+        var imageLoader = new Image();
+        imageLoader.src = "1.png"
+
+        const wid = imageLoader.width;
+        const hei = imageLoader.height;
+
+        const preprocessedData = preprocess(imageLoader, wid, hei);
+        const inputTensor = new onnx.Tensor(preprocessedData, 'float32', [1, 3, wid, hei]);
+        const outputMap = await session.run([inputTensor]);
+        const outputData = outputMap.values().next().value.data;
+        var x = outputData.toString();
+        console.log(x);
+    }
--- src/onnxjs/test/unittests/api/inference-session.ts
+++
@@ -1,4 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-// TODO: Inference session test cases
