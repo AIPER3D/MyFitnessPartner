29b606463f3202e5640ceb2c8d125dfd53355db5
fsnyd <52208468+fsnyd@users.noreply.github.com>
2021-04-22 22:34:02 +0900

    으아ㅏㅏㅏㅏㅏㅏㅏㅏ

--- src/back/test.ts
+++ src/back/test.ts
@@ -1,8 +1,21 @@
-const cv = require('opencv4nodejs');
+// const cv = require('opencv4nodejs');
+// const mat = cv.imreadAsync(__dirname+'/1.png', (err:any, mat:any)
+// {
+//     cv.imshow('a window name', mat);
+//     cv.waitKey();
+// });
+const { spawn } = require('child_process');
+const childPython = spawn('python', ['--version']);
 
-const mat = cv.imreadAsync(__dirname+'/1.png', (err:any, mat:any) => 
-{
-    cv.imshow('a window name', mat);
-    cv.waitKey();
+childPython.stdout.on('data', (data:any)=> {
+	console.log('stdout: '+ data);
+});
+
+childPython.stderr.on('data', (data:any)=> {
+	console.error('stdout: '+ data);
+});
+
+childPython.on('close', (code:any)=> {
+	console.log('child process exited with code  '+ code);
 });
 
