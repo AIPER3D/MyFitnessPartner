f39b4486d3dd593970284df498ea3883d8966635
ToTo-Mo <bum4496@naver.com>
2021-05-01 21:15:59 +0900

    Add posenet

--- src/components/exercisePlayer/webcam.tsx
+++ src/components/exercisePlayer/webcam.tsx
@@ -1,42 +1,55 @@
 /* eslint-disable new-cap */
 /* eslint-disable no-constant-condition */
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useReducer } from 'react';
 import styled from 'styled-components';
-
 import * as tf from '@tensorflow/tfjs';
-import { InferenceSession, Tensor } from 'onnxjs';
+import * as posenet from '@tensorflow-models/posenet';
+import { Stage, Sprite, Graphics } from '@inlet/react-pixi';
+import { Container } from 'pixi.js';
+import { useCallback } from 'react';
+import { useState } from 'react';
 
 type Props = {
 	width: number;
 	height: number;
+	opacity: number;
 }
 
-const Wrapper = styled.video``;
-
-function Webcam({ width, height } : Props) {
+function Webcam({ width, height }: Props) {
 	let timer: any = null;
-	const session = new InferenceSession({ backendHint: 'webgl' });
 	const elementRef = useRef<HTMLVideoElement>(null);
 	const webcamRef = useRef<any>(null);
+	let net: any;
+
+	const inputHeight = 256;
+	const inputWidth = 512;
+
+	const upScaleRatio = width / inputWidth;
+
+	const [keypoints, setKeypoints] = useState<any>(null);
 
 	async function load() {
-		const file = window.api.fs.readFileSync('src/model/human-pose-estimation-3d.onnx');
-		const uint8Array = new Uint8Array(file);
-
-		await session.loadModel(uint8Array);
-		console.log('model loaded');
+		net = await posenet.load({
+			architecture: 'MobileNetV1',
+			outputStride: 16,
+			inputResolution: { width: inputWidth, height: inputHeight },
+			multiplier: 1,
+			quantBytes: 2,
+		});
 	}
+
 	async function run() {
 		if (elementRef.current == null) return;
 		const element = elementRef.current;
 
 		webcamRef.current = await tf.data.webcam(element, {
-			resizeHeight: 256,
-			resizeWidth: 448,
+			resizeHeight: inputHeight,
+			resizeWidth: inputWidth,
 		});
 
-		timer = setInterval(capture, 500);
+		timer = setInterval(capture, 100);
 	}
+
 	async function capture() {
 		if (webcamRef.current == null) return;
 		const webcam = webcamRef.current;
@@ -44,42 +57,23 @@ function Webcam({ width, height } : Props) {
 		// 1. caputer iamge
 		const image = await webcam.capture();
 
-		// 2. Float32Array to onnx Tensor
-		const inputArray = image.reshape([3, 256, 448]).dataSync();
+		// 2. inference iamge
+		const pose = await net.estimateSinglePose(image, {
+			flipHorizontal: false,
+		});
 
-		// 3. [batch, channel, height, wdith]
-		const inputTensor = new Tensor(inputArray, 'float32', [1, 3, 256, 448]);
+		// 3. upscale keypoints to webcam resolution
+		pose.keypoints.map( (keypoints : any) => {
+			keypoints.position.x *= upScaleRatio;
+			keypoints.position.y *= upScaleRatio;
+		});
 
-		const output = await session.run([inputTensor]);
+		// 4. set keypoints
+		setKeypoints(pose.keypoints);
+
+		// 5. keypoitns to ipc
+
 		image.dispose();
-
-		// console.log(output.keys());
-
-		const features = output.get('features');
-		const partAffinityField = output.get('pafs');
-		const heatmaps = output.get('heatmaps');
-
-		if (features && partAffinityField && heatmaps) {
-			const fdim = features?.dims;
-
-			const featuresTensor =
-				tf.tensor4d(
-					features.data, [fdim[0], fdim[1], fdim[2], fdim[3]], 'float32');
-
-			const pdim = partAffinityField?.dims;
-
-			const partAffinityFieldTensor =
-				tf.tensor4d(
-					partAffinityField?.data, [pdim[0], pdim[1], pdim[2], pdim[3]], 'float32');
-
-			const hdim = heatmaps.dims;
-
-			const heatmapsTensor =
-				tf.tensor4d(
-					heatmaps?.data, [hdim[0], hdim[1], hdim[2], hdim[3]], 'float32');
-		}
-
-		// 1차원 배열에 차원 값을 가진 데이터
 		await tf.nextFrame();
 	}
 
@@ -94,15 +88,62 @@ function Webcam({ width, height } : Props) {
 		};
 	}, [elementRef]);
 
+	const drawKeypoints = React.useCallback((graphics) => {
+		graphics.clear();
+
+		if (!keypoints) return;
+
+		for (let i=0; i<keypoints.length; i++) {
+			const x = keypoints[i].position.x;
+			const y = keypoints[i].position.y;
+
+			graphics.beginFill(0x3970E3);
+			graphics.drawCircle(x, y, 5);
+			graphics.endFill();
+		}
+	}, [keypoints]);
+
+	const stageProps = {
+		width: width,
+		height: height,
+		options: {
+			backgroundAlpha: 0,
+			antialias: true,
+			backgroundColor: 0x000000,
+		},
+	};
+
 	return (
-		<Wrapper
-			muted
-			autoPlay
-			ref = { elementRef }
-			width = { width }
-			height = { height }
-		/>
+		<Wrapper>
+			<Video
+				muted
+				autoPlay
+				ref={elementRef}
+				width={width}
+				height={height}
+			/>
+
+			<SStage {...stageProps}>
+				<Graphics draw={drawKeypoints}/>
+			</SStage>
+
+		</Wrapper>
+
 	);
 }
+
+const Wrapper = styled.div`
+	opacity: 0.8;
+`;
+const Video = styled.video``;
+
+const SStage = styled(Stage)`
+	position: absolute;
+
+	top: 0px;
+	left: 0px;
+
+	/* z-index: 1001; */
+`;
 
 export default Webcam;
