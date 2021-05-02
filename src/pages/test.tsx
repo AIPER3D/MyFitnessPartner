de72c58a2b09e38cff768b2ad8216170f9b82f7d
ToTo-Mo <bum4496@naver.com>
2021-05-02 16:03:02 +0900

    Add model and tensorflow utils

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,18 +2,39 @@ import React, { useEffect, useState } from 'react';
 import Calendar from '../components/calendar/calendar';
 import { RxDatabase } from 'rxdb';
 
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import { imageFromVideo } from '../util/video-util';
+
 type PageProps = {
 	db: RxDatabase;
 };
 
 function Main({ db } : PageProps) {
-	function load(seq : number) {
+	function load() {
 		const file = window.api.fs.readFileSync('./files/videos/');
 		const uint8Array = new Uint8Array(file);
 		const arrayBuffer = uint8Array.buffer;
 		const blob = new Blob([arrayBuffer]);
 		const url = URL.createObjectURL(blob);
 	}
+
+	const run = async () => {
+		// 1. model load
+		const model = await mobilenet.load();
+
+		// 2. get image from video
+		const image = tf.tensor3d([3, 3, 3]);
+
+		// 3. image to tensor
+		model.classify(image);
+	};
+
+	useEffect( () => {
+		load();
+
+		run();
+	}, []);
 
 	// return (
 	// 	<Container>
