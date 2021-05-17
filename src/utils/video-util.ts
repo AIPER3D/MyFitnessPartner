5169f342b2acc738e43a7778b523772dc057e47b
ToTo-Mo <bum4496@naver.com>
2021-05-18 00:34:36 +0900

    Add tensor to image function

--- src/utils/video-util.ts
+++ src/utils/video-util.ts
@@ -3,6 +3,34 @@ import * as tf from '@tensorflow/tfjs';
 const canvas : HTMLCanvasElement = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
+export function tensorToImage(tensor : any) {
+	const [batch, height, width, channel] = tensor.shape;
+
+	// create a buffer array
+	const buffer = new Uint8ClampedArray(width * height * 4);
+	// create an Image data var
+	const imageData = new ImageData(width, height);
+	// get the tensor values as data
+	const data = tensor.dataSync();
+	// map the values to the buffer
+
+	var i = 0;
+	for (var y = 0; y < height; y++) {
+		for (var x = 0; x < width; x++) {
+			var pos = (y * width + x) * 4; // position in buffer based on x and y
+			buffer[pos] = data[i]; // some R value [0, 255]
+			buffer[pos+1] = data[i+1]; // some G value
+			buffer[pos+2] = data[i+2]; // some B value
+			buffer[pos+3] = 255; // set alpha channel
+			i+=3;
+		}
+	}
+	// set the buffer to the image data
+	imageData.data.set(buffer);
+
+	return imageData;
+}
+
 const imageFromVideo = (video : HTMLVideoElement) => {
 	canvas.width = video.videoWidth;
 	canvas.height = video.videoHeight;
