0137f0b6a865ff738d59d4ecdf42bdbdad703812
Herase <phoebe7075@gmail.com>
2021-07-15 13:52:10 +0900

    DoughnutChart.tsx add Comment
    컴포넌트에 개발의도와 각 메소드의 작동 방식에 대한 주석 추가

--- src/components/dashboard/DoughnutChart.tsx
+++ src/components/dashboard/DoughnutChart.tsx
@@ -4,10 +4,14 @@ import './DoughnutChart.scss';
 type Props = {
 	list: any;
 }
+/* 구현자 : 김인환
+Chart.tsx와 마찬가지로 react-vis API를 활용하여 만든 도넛 그래프 컴포넌트이다.
+운동의 비율을 통해 얼마나 적절한 비율로 운동하는지를 나타내는 것이 필요하다 생각해서 구현하게 되었다.
+*/
 function DoughnutChart({list} : Props) {
 	let data : any = [];
 	if (list) {
-		if (list.Squat === 0 && list.Jump === 0 && list.Lunge === 0) {
+		if (list.Squat === 0 && list.Jump === 0 && list.Lunge === 0) { // 비동기이기 때문에 데이터가 없을 경우의 초기값을 세팅해 주었다.
 			data = [
 				{angle: 1, radius: 1.7, color: 1, padAngle: 0, className: 'first'},
 			];
